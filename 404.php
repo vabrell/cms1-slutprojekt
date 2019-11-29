1c70a3a68abf71416e0a4e89243fbff9483ef3b6
Victor Abrell <victor@abrell.se>
2019-11-29 14:49:59 +0100

    Lagt till 404.php som visas om sidan man försökte nå inte kunde hittas

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+// Hämta header.php
+get_header();
+?>
+
+<div class="container">
+  <div class="row mt-5 text-muted">
+    <div class="col">
+      <h1><?php _e('The page could not be found'); ?>!</h1>
+    </div>
+  </div>
+</div>
+
+<?php
+// Hämta footer.php
+get_footer();
