89ca10635b9bd1eeaeaa0eaba4c82b4b429b3d2f
Victor Abrell <victor@abrell.se>
2019-11-29 13:31:38 +0100

    Lagt till innehåll på startside-mallen

--- front-page.php
+++ front-page.php
@@ -3,6 +3,21 @@
 get_header();
 ?>
 
+<div class="container">
+  <?php
+    // Kolla om det finns någon post i databasen att hämta
+    if (have_posts()) {
+      // Medans det finns poster i databasen loopa igenom dem
+      while (have_posts()) {
+        // Välj posten och ta bort den ur listan
+        the_post();
+
+        // Skriv ut allt innehåll
+        the_content();
+      }
+    }
+  ?>
+</div>
 
 <?php
 // Hämta footer.php
