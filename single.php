1cc36ddb5fa526e88e9b5ff81939eb2148adc580
Victor Abrell <victor@abrell.se>
2019-11-29 14:44:31 +0100

    Lagt till mallen för enskilda inlägg

---
+++ single.php
@@ -0,0 +1,76 @@
+<?php
+// Hämta header.php
+get_header();
+?>
+
+<div class="container-fluid">
+  <?php
+  // Kolla om det finns någon post i databasen att hämta
+  if (have_posts()) {
+    // Medans det finns poster i databasen loopa igenom dem
+    while (have_posts()) {
+      // Välj posten och ta bort den ur listan
+      the_post();
+      ?>
+
+      <div class="row mb-3">
+        <div class="col p-3">
+          <h1>
+            <?php
+                // Hämta länken till posten
+                ?>
+            <a href="<?php echo get_the_permalink(); ?>">
+              <?php
+                  // Hämta titeln på posten
+                  echo get_the_title();
+                  ?>
+            </a>
+          </h1>
+          <div class="bg-light text-muted small">
+            <div class="container-fluid">
+              <div class="row">
+                <?php
+                    // Hämta författaren
+                    echo get_the_author_posts_link();
+                    ?>
+                &nbsp;@&nbsp;
+                <?php
+                    // Hämta länk till datum arkiv
+                    $post_date = explode('-', get_the_date('Y-m-d'));
+                    $url = get_day_link($post_date[0], $post_date[1], $post_date[2]);
+                    ?>
+                <a href="<?php echo $url; ?>">
+                  <?php
+                      // Hämta datum
+                      echo get_the_date();
+                      ?>
+                </a>
+              </div>
+              <div class="row">
+                <?php
+                    // Hämta kategorier
+                    the_category(', ');
+                    ?>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-2">
+            <?php
+              // Hämta innehållet på posten
+              echo get_the_content();
+            ?>
+          </div>
+
+        </div>
+      </div>
+
+  <?php
+    }
+  }
+  ?>
+</div>
+
+<?php
+// Hämta footer.php
+get_footer();
