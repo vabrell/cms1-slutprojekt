a8413d29ebdbf4facda7e45b726c04426e3fadd2
Victor Abrell <victor@abrell.se>
2019-12-02 20:39:44 +0100

    Lägger till utvald bild om det finns någon

--- template-recipes.php
+++ template-recipes.php
@@ -16,66 +16,79 @@ $recipes = new WP_query([
 
 <div class="container">
   <?php
-  // Kolla om det finns någon post i databasen att hämta
+  // Kolla om det finns något recept i databasen att hämta
   if ($recipes->have_posts()) {
-    // Medans det finns poster i databasen loopa igenom dem
+    // Medans det finns recept i databasen loopa igenom dem
     while ($recipes->have_posts()) {
-      // Välj posten och ta bort den ur listan
+      // Välj receptet och ta bort den ur listan
       $recipes->the_post();
       ?>
-
-      <div class="row mb-3">
-        <div class="col p-3 shadow">
-          <h1>
-            <?php
-                // Hämta länken till posten
-                ?>
-            <a href="<?php echo get_the_permalink(); ?>">
+      <div class="row shadow p-3 mb-5">
+        <div class="row">
+          <div class="col">
+            <h1>
               <?php
-                  // Hämta titeln på posten
-                  echo get_the_title();
-                  ?>
-            </a>
-          </h1>
-          <p class="lead p-2">
-            <?php
-                // Kolla om inställningen säger att hela innehållet skall visas eller inte
-                if (get_option('rss_use_excerpt')) {
-                  echo get_the_excerpt();
-                }
-                // Hämta innehållet på posten
-                else {
-                  echo get_the_content();
-                }
-                ?>
-          </p>
-          <div class="bg-light text-muted small">
-            <div class="container">
-              <div class="row">
+                // Hämta länken till receptet
+              ?>
+              <a href="<?php echo get_the_permalink(); ?>">
                 <?php
+                  // Hämta titeln på receptet
+                  echo get_the_title();
+                ?>
+              </a>
+            </h1>
+          </div>
+        </div>
+        <div class="row">
+          <?php
+            // Kolla om receptet har någon bild
+            if (has_post_thumbnail()) {
+          ?>
+
+          <div class="col-3">
+            <img class="img-fluid" src="<?php echo get_the_post_thumbnail_url(); ?>">
+          </div>
+
+          <?php
+            }
+          ?>
+          <div class="col-9">
+            <p class="lead p-2">
+              <?php
+              // Hämta beskrivningen om receptet
+                echo get_the_excerpt();
+              ?>
+            </p>
+          </div>
+          <div class="col">
+            <div class="bg-light text-muted small">
+              <div class="container">
+                <div class="row">
+                  <?php
                     // Hämta författaren
                     echo get_the_author_posts_link();
-                    ?>
-                &nbsp;@&nbsp;
-                <?php
+                  ?>
+                  &nbsp;@&nbsp;
+                  <?php
                     // Hämta länk till datum arkiv
                     $post_date = explode('-', get_the_date('Y-m-d'));
                     $url = get_day_link($post_date[0], $post_date[1], $post_date[2]);
-                    ?>
-                <a href="<?php echo $url; ?>">
-                  <?php
+                  ?>
+                  <a href="<?php echo $url; ?>">
+                    <?php
                       // Hämta datum
                       echo get_the_date();
-                      ?>
-                </a>
-              </div>
-              <div class="row">
-                <?php
-                  _e('Mealtime:', 'sp');
-                  echo "&nbsp;";
-                    // Hämta kategorier
-                    the_terms(get_the_ID(),'meal');
                     ?>
+                  </a>
+                </div>
+                <div class="row">
+                  <?php
+                    _e('Mealtime:', 'sp');
+                    echo "&nbsp;";
+                    // Hämta kategorier
+                    the_terms(get_the_ID(), 'meal');
+                  ?>
+                </div>
               </div>
             </div>
           </div>
