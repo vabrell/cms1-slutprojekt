bc00b6daad9d39748601faf36d5ea29836db3b90
Victor Abrell <victor@abrell.se>
2019-12-03 13:26:21 +0100

    Lagt till en arkivmall för författare då den behöver få med recepten också

---
+++ author.php
@@ -0,0 +1,93 @@
+<?php
+// Hämta header.php
+get_header();
+
+// Hämta den globala datan för författaren
+global $authordata;
+if (!is_object($authordata)) {
+  return '';
+}
+
+$auhtor_posts = new WP_query([
+  'post_type' => ['post', 'recipe'],
+  'author' => $authordata->ID
+]);
+?>
+
+<div class="container">
+  <h1>
+    <?php
+    // Hämta titeln för vilket arkiv det är
+    echo get_the_archive_title();
+    ?>
+  </h1>
+  <?php
+  // Kolla om det finns någon post i databasen att hämta
+  if ($auhtor_posts->have_posts()) {
+    // Medans det finns poster i databasen loopa igenom dem
+    while ($auhtor_posts->have_posts()) {
+      // Välj posten och ta bort den ur listan
+      $auhtor_posts->the_post();
+      ?>
+
+      <div class="row mb-5 p-3 shadow">
+        <div class="col-12">
+          <h1>
+            <?php
+            // Hämta länken till posten
+            ?>
+            <a href="<?php echo get_the_permalink(); ?>">
+              <?php
+                // Hämta titeln på posten
+                echo get_the_title();
+              ?>
+            </a>
+          </h1>
+        </div>
+        <div class="col-9">
+          <p class="lead p-2">
+            <?php
+                // Hämta beskrivningen
+                echo get_the_excerpt();
+                ?>
+          </p>
+        </div>
+        <div class="col-12">
+          <div class="bg-light text-muted small">
+            <div class="container">
+              <div class="row">
+                  <?php
+                      echo __('Author', 'sp') . ':&nbsp;';
+                      // Hämta författaren
+                      echo get_the_author_posts_link();
+                      ?>
+              </div>
+              <div class="row">
+                <?php
+                    // Hämta kategorier
+                    if (count(get_the_category()) > 0) {
+                      echo __('Category', 'sp') . ':&nbsp;';
+                      the_category(', ');
+                    }
+
+                    // Hämta måltider
+                    if (has_term('', 'meal')) {
+                      echo __('Mealtime', 'sp') . ':&nbsp;';
+                      the_terms(get_the_ID(), 'meal');
+                    }
+                    ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  <?php
+    }
+  }
+  ?>
+</div>
+
+<?php
+// Hämta footer.php
+get_footer();
